bf55b24cee463df06ff22449095e6f5b2e43a5c5
Cal <calvin36@gmail.com>
2026-08-12 17:56:29 +0100

    make sendIndex for gcloud schedule

--- sendIndex/sendIndex.sh
+++ sendIndex/sendIndex.sh
@@ -1,14 +1,7 @@
-#/bin/bash
+#!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "Error: Missing required parameters."
-    echo "Usage: $0 <user>"
-    exit 1
-fi
+indexData=$(curl -s "$APPSERVER_URL/mindex")
 
-user="$1"
-
-indexData=$(curl -s http://localhost:3002/mindex)
 index=$(echo "$indexData" |
   jq -r '.[] | [.desp, .last, (.changesign + .change)] | @tsv' |
   awk -F'\t' '{
@@ -18,9 +11,9 @@ index=$(echo "$indexData" |
     printf "%s%s %6s %7s\n", $1, pad, $2, $3
   }'
 )
-#echo "$index"
+
 curl -G \
   --data-urlencode "msg=<code>$index</code>" \
-  --data-urlencode "id=$user" \
-  "http://localhost:3001/bot/broadcast"
+  --data-urlencode "id=$USERID" \
+  "$BOT_URL/bot/broadcast"
 
